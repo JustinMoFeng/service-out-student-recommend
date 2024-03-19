0702d52c1ee39c10570017f414af9f22bc7d08c4
mofengyichen <3309113612@qq.com>
2024-03-19 17:39:36 +0800

    feat: find all with '实习'

---
+++ find_shixi.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pprint import pprint
+import re
+
+data = pd.read_csv('E:\\race\\service_out\\data\\new_data.csv', header='infer', delimiter=',', encoding='utf-8')
+data.fillna('', inplace=True)
+array = data.values
+
+# 找到所有的实习
+dataSet = set()
+for i in range(len(array)):
+    # 如果array[i][5]中包含'实习'，则将这一条放入dataSet中】
+    pattern = '实习[^经期工项0123456789]'
+    pattern = re.compile(pattern)
+    if re.search(pattern, array[i][5]) is not None or '实习' in array[i][1]:
+        dataSet.add(i)
+
+# 按照dataSet中的序号进行查找，将其中包含编号的信息存入csv
+newData = []
+for i in dataSet:
+    newData.append(array[i])
+
+# 将newData存入csv
+newData = pd.DataFrame(newData, columns=data.columns)
+newData.to_csv('E:\\race\\service_out\\data\\shixi.csv', index=False, encoding='utf-8')
